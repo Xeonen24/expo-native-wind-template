c440d2d4c9997255fe8bb86d7c19652703a70fc2
Alpesh Pote <108137169+Xeonen24@users.noreply.github.com>
2025-09-26 16:33:08 +0530

    Refactor code structure for improved readability and maintainability

---
+++ screens/home.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const home = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">Welcome to Nativewind!</Text>
+    </View>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({});
